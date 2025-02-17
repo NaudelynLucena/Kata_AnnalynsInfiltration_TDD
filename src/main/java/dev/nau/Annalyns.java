a5bd90b226d7f54bdf887476a1ba0fb390515d7c
NaudelynLucena <naulunau@gmail.com>
2025-02-18 00:37:27 +0100

    Implement  method. Test pass

--- src/main/java/dev/nau/Annalyns.java
+++ src/main/java/dev/nau/Annalyns.java
@@ -5,4 +5,8 @@ public class Annalyns {
     public static boolean canFastAttack(boolean knightIsAwake) {
         return (!knightIsAwake);
     }
+
+    public static boolean canSpy(boolean knightIsAwake, boolean archerIsAwake, boolean prisonerIsAwake) {
+        return (knightIsAwake || archerIsAwake || prisonerIsAwake);
+    }
 }
