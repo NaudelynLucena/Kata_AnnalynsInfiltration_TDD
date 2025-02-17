8896c41c8b2ca3c2cfe72541f94439f05a03c077
NaudelynLucena <naulunau@gmail.com>
2025-02-18 00:34:23 +0100

    Implemented parameterized tests to cover different combinations of awake statuses for knight, archer, and prisoner. Tests fail.

--- src/test/java/dev/nau/AnnalynsTest.java
+++ src/test/java/dev/nau/AnnalynsTest.java
@@ -2,7 +2,7 @@ package dev.nau;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
-
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
@@ -23,5 +23,22 @@ public class AnnalynsTest {
     public void cannot_execute_fast_attack_if_knight_is_awake() {
         boolean knightIsAwake = true;
         assertThat(Annalyns.canFastAttack(knightIsAwake), is(false));
+    }
+
+    @org.junit.jupiter.params.ParameterizedTest
+    @CsvSource({
+            "false, false, true, true",
+            "false, true, true, true",
+            "true, true, true, true",
+            "true, false, true, true",
+            "true, true, false, true",
+            "false, false, false, false",
+            "true, false, false, true",
+            "false, true, false, true"
+    })
+    public void testCanSpy(boolean knightIsAwake, boolean archerIsAwake, boolean prisonerIsAwake,
+            boolean expectedResult) {
+        // Aquí el método puede ser llamado con los parámetros
+        assertThat(Annalyns.canSpy(knightIsAwake, archerIsAwake, prisonerIsAwake), is(expectedResult));
     }
 }
