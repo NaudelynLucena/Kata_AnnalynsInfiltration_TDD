771ac67b23391fd4d2467313e6662998b3d19d98
NaudelynLucena <naulunau@gmail.com>
2025-02-17 23:14:21 +0100

    Implement tests for canFastAttack method to verify behavior based on knight's state. Tests fail

---
+++ src/test/java/dev/nau/AnnalynsTest.java
@@ -0,0 +1,16 @@
+package dev.nau;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+public class AnnalynsTest {
+
+    @Test
+    @Tag("task:1")
+    @DisplayName("The canFastAttack method returns true when knight is sleeping")
+    public void can_execute_fast_attack_if_knight_is_sleeping() {
+        boolean knightIsAwake = false;
+        assertThat(Annalyns.canFastAttack(knightIsAwake), is(true));
+    }
+}
